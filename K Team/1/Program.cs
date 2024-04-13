9ff6753c8663cc0afd9a40e660193bbe95f61fda
itzaRinna <dutchismahdick@gmail.com>
2024-04-13 13:26:12 +0700

    1 and 2

---
+++ K Team/1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FirstProgramme
+{
+    /// <summary>
+    /// Comment cho class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Comment cho hàm - hàm chạy đầu tiên khi chương trình được biên dịch
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Comment cho 1 dòng
+            
+            Console.WriteLine("K Team"); //Hoàn toàn có thể comment như thế này
+            
+            Console.ReadKey();
+            
+            /*Comment*/
+            /*
+             *  Hay như thế này
+             */
+        } // đoạn code/chữ bạn viết phía sau dấu // sẽ không được biên dịch. Nhưng đoạn code phía trước đó vẫn được chạy 
+    }
+}
