0c9435d213152bf7e66e611231b19e7c0721c04b
itzaRinna <dutchismahdick@gmail.com>
2024-04-17 12:23:35 +0700

    new

--- K Team/7/Program.cs
+++ K Team/7/Program.cs
@@ -17,41 +17,82 @@ namespace SeventhProgramme
 
             #region Chuyển kiểu tường minh
 
-            int i = 300; // 300 có mã nhị phân là 100101100
-            byte b = (byte)i; // do kiểu byte có giới hạn đến giá trị 225
-                              // nên không thể chứa số 300 được mà kiểu byte có kích thước là 1 byte tương đương 8 bit.
-                              // Như vậy ta cần cắt mã nhị phân của số 300 về còn 8 bit là được.
-                              // Mã nhị phân 300 là 100101100 cắt từ trái qua 1 bit ta được 00101100 (đủ 8 bit)
-                              // tương đương với số 44. Cuối cùng biến b sẽ mang giá trị là 44.  
-
-            Console.WriteLine(" i = " + i);
-            Console.WriteLine(" b = " + b);
-
-            int a = 5;
-            int c = 2;
-            float tt = (float)a / c; // =>2.5
-            
-            double d = 2 / 3; // kết quả ra 0 vì 2 và 3 đều là số nguyên nên thực hiện 2 chia lấy phần nguyên với 3 được 0
-            double k = (double)2 / 3; // Ép kiểu số 2 từ kiu nguyên sang kiểu số thực . Như vậy kết quả phép chia sẽ ra số thực
-            double t = 1.0 * 2 / 3;
-
-            Console.WriteLine(" d = {0} \n k = {1} \n t = {2}", d,k,t);
-
-            Console.ReadKey();
+            // int i = 300; // 300 có mã nhị phân là 100101100
+            // byte b = (byte)i; // do kiểu byte có giới hạn đến giá trị 225
+            //                   // nên không thể chứa số 300 được mà kiểu byte có kích thước là 1 byte tương đương 8 bit.
+            //                   // Như vậy ta cần cắt mã nhị phân của số 300 về còn 8 bit là được.
+            //                   // Mã nhị phân 300 là 100101100 cắt từ trái qua 1 bit ta được 00101100 (đủ 8 bit)
+            //                   // tương đương với số 44. Cuối cùng biến b sẽ mang giá trị là 44.  
+            //
+            // Console.WriteLine(" i = " + i);
+            // Console.WriteLine(" b = " + b);
+            //
+            // int a = 5;
+            // int c = 2;
+            // float tt = (float)a / c; // =>2.5
+            //
+            // double d = 2 / 3; // kết quả ra 0 vì 2 và 3 đều là số nguyên nên thực hiện 2 chia lấy phần nguyên với 3 được 0
+            // double k = (double)2 / 3; // Ép kiểu số 2 từ kiu nguyên sang kiểu số thực . Như vậy kết quả phép chia sẽ ra số thực
+            // double t = 1.0 * 2 / 3; // Thực hiện nhân 1.0 với 2 mục đích để biến số 2 ( số nguyên ) thành 2.0 (số thực)
+            //
+            // Console.WriteLine(" d = {0} \n k = {1} \n t = {2}", d,k,t);
+            //
+            // Console.ReadKey();
 
             #endregion
 
             #region Parse()
 
-
+            // string s = "10";
+            // int k = int.Parse(s); // Chuyễn chuỗi s sang kiểu int và lưu giá trị vào biến k - Kết quả k = 10
+            // double e = double.Parse("10.9"); // Chuyến chuỗi giá trị hằng "10.9" sang kiểu int và lưu giá trị vào biến e - Kết quả e = 10.9 
 
             #endregion
 
             #region TryParse()
 
-
+            // int Result; // Biến chứa giá trị kết quả khi ép kiểu thành công
+            // bool isSuccess; // Biến kiểm tra việc ép kiểu có thành công hay không
+            // string Data1 = "10", Data2 = "Kteam"; // Dữ liệu cần ép kiểu
+            //
+            // isSuccess = int.TryParse(Data1, out Result); // Thử ép kiểu Data1 về int nếu thành công thì Result sẽ chứa giá trị kết quả ép kiểu và isSuccess sẽ mang giá trị true. Ngược lại Result sẽ mang giá trị 0 và isSuccess mang giá trị false
+            // Console.WriteLine(isSuccess == true? "Success !" : "Failed!"); // Sử dụng toán tử 3 ngôi để in ra màn hình việc ép kiểu đã thành công hay thất bại.
+            // Console.WriteLine(" Result = " + Result); // In giá trị Result ra màn hình
+            //
+            // isSuccess = int.TryParse(Data2, out Result); // Tương tự như trên nhưng với Data2
+            // Console.WriteLine(isSuccess == true? "Success !" : "Failed!"); // Tương tự như trên
+            // Console.WriteLine(" Result = " + Result); // Tương tự như trên
 
             #endregion
+            
+            int A, B; // Biến chứa giá trị 2 số vừa nhập vào (kiểu số)
+            int Tong, Hieu, Tich; // Biến chứa kết quả tổng, hiệu, tích
+            double Thuong; // Vì phép chia có thể cho ra số thập phân nên dùng biến kiểu double để chứa nó.
+            string strA, strB; // Biến chứa giá trị 2 số nhập vào từ bàn phím (kiểu chuỗi)
+
+            Console.WriteLine("         **************************************************");
+            Console.WriteLine("         *                                         			       *");
+            Console.WriteLine("         *   Chuong trinh tinh tong, hieu, tich, thuong  *");
+            Console.WriteLine("         *                                            		       *");
+            Console.WriteLine("         **************************************************");
+
+            Console.Write("\n Moi ban nhap so A: ");
+            strA = Console.ReadLine(); // Nhận giá trị nhập vào từ bàn phím cho số A
+            Console.Write(" Moi ban nhap so B: ");
+            strB = Console.ReadLine(); // Nhận giá trị nhập vào từ bàn phím cho số B
+
+            A = int.Parse(strA); // Ép kiểu giá trị nhập vào từ kiễu chuỗi sang kiểu số nguyên, sử dụng phương thức Parse()
+            B = int.Parse(strB); // Tương tự
+
+            Tong = A + B;
+            Hieu = A - B;
+            Tich = A * B;
+            Thuong = (double)A / B; // Ép kiểu số A về số thập phân để phép chia cho ra số thập phân
+
+            Console.WriteLine(" Tong = " + Tong);
+            Console.WriteLine(" Hieu = " + Hieu);
+            Console.WriteLine(" Tich = " + Tich);
+            Console.WriteLine(" Thuong = " + Thuong);
 
         }
     }
---
+++ K Team/9/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Ninth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Switch dạng thiếu
+            
+            // int k = 10;
+            //
+            // switch (k)
+            // {
+            //     case 3:
+            //         Console.WriteLine("HowKteam");
+            //         break; // Vì case này có lệnh thực hiện nên phải có lệnh break 
+            //     case 9:
+            //         Console.WriteLine("Kteam");
+            //         break;
+            //     case 10:
+            //         Console.WriteLine("Free Education");
+            //         break;
+            // }
+            
+            #endregion
+
+			#region Switch dạng đủ
+            
+            // int k = 8;
+            //
+            // switch (k)
+            // {
+            //     case 3:
+            //         Console.WriteLine("HowKteam");
+            //         break;
+            //     case 9:
+            //         Console.WriteLine("Kteam");
+            //         break;
+            //     case 10:
+            //         Console.WriteLine("Free Education");
+            //         break;
+            //     default: // Nếu không thỏa các trường hợp trên sẽ thực hiện lệnh sau đây
+            //         Console.WriteLine("Connecting to HowKteam. . .");
+            //         break;
+            // }
+            
+			#endregion
+            
+            int Year; // Biến chứa giá trị năm cần tính.
+            string Can = "", Chi = ""; // Biến chứa kết quả.
+
+            Console.Write(" Moi ban nhap mot nam bat ky: ");
+            Year = Int32.Parse(Console.ReadLine()); // Nhập năm dương lịch và ép kiểu về kiểu số nguyên
+            
+            switch (Year % 10) // Tìm Can như thuật toán đã trình bày.
+            {
+                case 0: // Mỗi case này tương ứng một kết quả cần tra cứu trong bảng tra cứu Can
+                    Can = "Canh"; // Giá trị tương ứng với mỗi case
+                    break;
+                case 1:
+                    Can = "Tan";
+                    break;
+                case 2:
+                    Can = "Nham";
+                    break;
+                case 3:
+                    Can = "Quy";
+                    break;
+                case 4:
+                    Can = "Giap";
+                    break;
+                case 5:
+                    Can = "At";
+                    break;
+                case 6:
+                    Can = "Binh";
+                    break;
+                case 7:
+                    Can = "Dinh";
+                    break;
+                case 8:
+                    Can = "Mau";
+                    break;
+                case 9:
+                    Can = "Ky";
+                    break;
+            }
+
+            switch (Year % 12) // Tìm Chi như thuật toán đã trình bày
+            {
+                case 0: // Mỗi case này tương ứng một kết quả cần tra cứu trong bảng tra cứu Chi
+                    Chi = "Than"; // Giá trị tương ứng với mỗi case
+                    break;
+                case 1:
+                    Chi = "Dau";
+                    break;
+                case 2:
+                    Chi = "Tuat";
+                    break;
+                case 3:
+                    Chi = "Hoi";
+                    break;
+                case 4:
+                    Chi = "Ty";
+                    break;
+                case 5:
+                    Chi = "Suu";
+                    break;
+                case 6:
+                    Chi = "Dan";
+                    break;
+                case 7:
+                    Chi = "Meo";
+                    break;
+                case 8:
+                    Chi = "Thin";
+                    break;
+                case 9:
+                    Chi = "Ti";
+                    break;
+                case 10:
+                    Chi = "Ngo";
+                    break;
+                case 11:
+                    Chi = "Mui";
+                    break;
+            }
+
+            Console.WriteLine("Nam {0} co nam am lich la: {1} {2}", Year, Can, Chi); // Nối Can và Chi lại để được năm âm lịch
+
+            
+            Console.ReadKey();
+        }
+    }
+}
