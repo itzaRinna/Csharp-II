87f776f885ad8c290c5f4e212cbcf4206fa47081
itzaRinna <dutchismahdick@gmail.com>
2024-04-18 18:48:21 +0700

    new

---
+++ K Team/19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _19thProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 5;
+
+            Console.WriteLine("Value before increased: {0}", value);
+            
+            IncreaseValue(ref value);
+            
+            Console.WriteLine("Value after increased: {0}", value);
+
+            Console.ReadKey();
+        }
+
+        static void IncreaseValue(ref int value)
+        {
+            value++;
+        }
+    }
+}
---
+++ K Team/20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _20thProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+
+            // Console.WriteLine("Value before increased: {0}", value);
+            
+            IncreaseValue(out value);
+            
+            Console.WriteLine("Value after increased: {0}", value);
+
+            Console.ReadKey();
+        }
+
+        static void IncreaseValue(out int value)
+        {
+            value = 0;
+            value++;
+        }
+    }
+}
