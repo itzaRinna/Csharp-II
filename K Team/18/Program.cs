45e9f61bfd373cd109aae892441895af824d03b0
itzaRinna <dutchismahdick@gmail.com>
2024-04-18 16:42:53 +0700

    new

---
+++ K Team/18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _18thProgramme
+{
+    class Program
+    {
+        // static int value = 5;
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(value);
+            //
+            // value = 10;
+            //
+            // PrintSomeThing();
+
+            // bool isTrue = true;
+            //
+            // if (isTrue)
+            // {
+            //     int value = 5;
+            //
+            //     Console.WriteLine(value);
+            // }
+            // else // Lỗi vì nó là biến cục bộ ko phải toàn 
+            // {
+            //     Console.WriteLine(value);
+            // }
+            
+            Console.ReadKey();
+        }
+
+        static void PrintSomeThing()
+        {
+            // int value = 9;
+            //
+            // Console.WriteLine(value);
+        }
+    }
+}
