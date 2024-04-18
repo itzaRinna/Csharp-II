3a7335dd05d10b14f09dd3d5252b03a724cb0c7e
itzaRinna <dutchismahdick@gmail.com>
2024-04-18 11:43:48 +0700

    new:

---
+++ K Team/12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TwelfthProgramme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // vòng lặp kết thúc khi đủ 10 lần
+            // Lặp 10 lần
+            // {
+            //     Console.WriteLine("HowKteam.com");
+            // }
+            // int i = 0;
+            // // vòng lặp kết thúc khi điều kiện dừng thỏa là i > 10 hay I == 11
+            // Lặp đến khi i > 10
+            // {
+            //     Console.WriteLine("HowKteam.com");
+            //     i++; // i tăng thêm một đơn vị [0, 1, 2, 3, .., 10, 11]
+            // }
+            // int i = 0;
+            // // vòng lặp không kết thúc vì i mãi == 0
+            // Lặp đến khi i > 10
+            // {
+            //     Console.WriteLine("HowKteam.com");
+            // }
+            
+            /*
+             * For
+             * While
+             * Do While
+             * Foreach
+             * Các vòng lặp không chính quy
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ K Team/13/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace ThirteenthProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region VD1
+            
+            // int a = 1;
+            // switch (a)
+            // {
+            //     case 1: // label case 1
+            //         Console.WriteLine("Case 1");
+            //         break;
+            //     case 2: // label case 2
+            //         Console.WriteLine("Case 2");
+            //         goto case 1; // dịch chuyển tới label case 1
+            //         break; // Đoạn code này thừa vì sẽ không bao giờ thực thi
+            //     case 3: // label case 3
+            //         Console.WriteLine("Case 3");
+            //         break;
+            // }
+
+            #endregion
+            
+            #region VD2
+
+            // int a = 2;
+            // switch(a)
+            // {
+            //     case 1:             // label case 1
+            //         Console.WriteLine("Case 1");
+            //         break;
+            //     case 2:             // label case 2
+            //         Console.WriteLine("Case 2");
+            //         goto case 1;    // dịch chuyển tới label case 1
+            //         break;          // Đoạn code này thừa vì sẽ không bao giờ thực thi
+            //     case 3:             // label case 3
+            //         Console.WriteLine("Case 3");
+            //         break;
+            // }
+
+            #endregion
+            
+            #region VD3
+
+            // int a = 1;
+            // // nếu a == 2
+            // if (a == 2)
+            // {
+            //     // dịch chuyển tới vị trí label a_Is_2
+            //     goto a_Is_2;
+            // }
+            //
+            // Console.WriteLine("A == 1");
+            // a_Is_2:
+            // Console.WriteLine("A == 2");
+
+            #endregion
+            
+            #region VD4
+
+            // int a = 2;
+            // // nếu a == 2
+            // if (a == 2)
+            // {
+            //     // dịch chuyển tới vị trí label a_Is_2
+            //     goto a_Is_2;
+            // }
+            //
+            // Console.WriteLine("A == 1");
+            // a_Is_2:
+            // Console.WriteLine("A == 2");
+
+
+            #endregion
+            
+            #region VD5
+
+            // int a = 2;
+            //
+            // Ifinity_Loop:
+            // // nếu a == 2
+            // if (a == 2)
+            // {
+            //     // dịch chuyển tới vị trí label a_Is_2
+            //     goto a_Is_2;
+            // }
+            //
+            // Console.WriteLine("A == 1");
+            // a_Is_2:
+            // Console.WriteLine("A == 2");
+            // goto Ifinity_Loop; // dịch chuyển tới vị trí label Ifinity_Loop
+
+            #endregion
+
+            #region VD6
+
+            string howKteamstr = "HowKteam.com";
+            int count = 0;
+            
+            beginLoop:
+
+            Console.WriteLine(howKteamstr);
+            count++;
+            
+            if(count < 10)
+                goto beginLoop;
+
+            Console.WriteLine("End loop");
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
